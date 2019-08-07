6e188f88ad0be03105b01c1b1bedaad49e7ee1d9
Andreas Gruenwald <andreas@grynwald.net>
2019-08-07 17:02:35 +0200

    test: Clean up BadgeTest
    * Derive output name for test cases from test parameters and thus remove the need to
      provide a unique id for test cases.
    * Use Xunit.Combinatorial package to generate test cases (using pairwise
      testing strategy)
    * Remove perviously introduced BadgeTestData class as it is not requried after all

--- src/GhBadgesSharp.Test/BadgeTest.cs
+++ src/GhBadgesSharp.Test/BadgeTest.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using System.Security.Cryptography;
+using System.Text;
 using ApprovalTests;
 using ApprovalTests.Namers;
 using ApprovalTests.Reporters;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace GhBadgesSharp.Test
 {
@@ -12,11 +12,14 @@ namespace GhBadgesSharp.Test
     {
         private class ApprovalNamer : UnitTestFrameworkNamer
         {
-            private readonly int m_Id;
+            private readonly string m_Id;
 
-            public override string Name => $"{base.Name}_{m_Id:000}";
+            public override string Name => $"{base.Name}_{m_Id}";
 
-            public ApprovalNamer(int id)
+            public ApprovalNamer(int id) : this(id.ToString("000"))
+            { }
+
+            public ApprovalNamer(string id)
             {
                 m_Id = id;
             }
@@ -25,92 +28,70 @@ namespace GhBadgesSharp.Test
         }
 
 
-
-        private class BadgeTestData : IXunitSerializable
-        {
-            public BadgeStyle Style { get; private set; }
-
-            public string LeftText { get; private set; }
-
-            public string RightText { get; private set; }
-
-            public string Color { get; private set; }
-
-            public string LabelColor { get; private set; }
-
-            public string LeftLink { get; private set; }
-
-            public string RightLink { get; private set; }
-
-            public BadgeTestData(BadgeStyle style, string leftText, string rightText, string color, string labelColor, string leftLink, string rightLink)
-            {
-                Style = style;
-                LeftText = leftText;
-                RightText = rightText;
-                Color = color;
-                LabelColor = labelColor;
-                LeftLink = leftLink;
-                RightLink = rightLink;
-            }
-
-            // parameterless constructor required by xunit serialization
-            public BadgeTestData()
-            { }
-
-
-            public void Deserialize(IXunitSerializationInfo info)
-            {
-                Style = Enum.Parse<BadgeStyle>(info.GetValue<string>(nameof(Style)));
-                LeftText = info.GetValue<string>(nameof(LeftText));
-                RightText = info.GetValue<string>(nameof(RightText));
-                Color = info.GetValue<string>(nameof(Color));
-                LabelColor = info.GetValue<string>(nameof(LabelColor));
-                LeftLink = info.GetValue<string>(nameof(LeftLink));
-                RightLink = info.GetValue<string>(nameof(RightLink));
-            }
-
-            public void Serialize(IXunitSerializationInfo info)
-            {
-                info.AddValue(nameof(Style), Style.ToString());
-                info.AddValue(nameof(LeftText), LeftText);
-                info.AddValue(nameof(RightText), RightText);
-                info.AddValue(nameof(Color), Color);
-                info.AddValue(nameof(LabelColor), LabelColor);
-                info.AddValue(nameof(LeftLink), LeftLink);
-                info.AddValue(nameof(RightLink), RightLink);
-            }
-        }
-
-
         [Theory]
-        //          id  template                leftText   rightText  color       labelColor    logo    logoPosition    logoWidth    link1                  link2
-        [InlineData(1, BadgeStyle.Flat, "Hello", "World", "yellow", null, null, null, null, null, null)]
-        [InlineData(2, BadgeStyle.Flat, "Hello", "World", "yellow", null, null, null, null, "http://example.com", null)]
-        [InlineData(3, BadgeStyle.Plastic, "Hello", "World", "yellow", null, null, null, null, null, null)]
-        [InlineData(4, BadgeStyle.Plastic, "Hello", "World", "yellow", null, null, null, null, "http://example.com", null)]
-        [InlineData(5, BadgeStyle.Flat, "Hello", "World", "yellow", null, null, null, null, "http://example.com", "https://www.github.com")]
-        [InlineData(6, BadgeStyle.Plastic, "Hello", "World", "yellow", null, null, null, null, "http://example.com", "https://www.github.com")]
-        [InlineData(7, BadgeStyle.Plastic, "Hello", "World", "yellow", null, null, null, null, "", null)]
-        [InlineData(8, BadgeStyle.Flat, "Hello", "World", "yellow", null, null, null, null, "", null)]
-        [InlineData(9, BadgeStyle.Flat, "Hello", "World", "red", "blue", null, null, null, null, null)]
-        [InlineData(10, BadgeStyle.Plastic, "Hello", "World", "red", "blue", null, null, null, null, null)]
-        [InlineData(11, BadgeStyle.FlatSquare, "Hello", "World", "yellow", null, null, null, null, null, null)]
-        [InlineData(12, BadgeStyle.FlatSquare, "Hello", "World", "yellow", null, null, null, null, "http://example.com", null)]
-        [InlineData(13, BadgeStyle.FlatSquare, "Hello", "World", "yellow", null, null, null, null, "http://example.com", "https://www.github.com")]
-        [InlineData(14, BadgeStyle.FlatSquare, "Hello", "World", "yellow", null, null, null, null, "", null)]
-        [InlineData(15, BadgeStyle.FlatSquare, "Hello", "World", "red", "blue", null, null, null, null, null)]
-        [InlineData(16, BadgeStyle.ForTheBadge, "Hello", "World", "yellow", null, null, null, null, null, null)]
-        [InlineData(17, BadgeStyle.ForTheBadge, "Hello", "World", "yellow", null, null, null, null, "http://example.com", null)]
-        [InlineData(18, BadgeStyle.ForTheBadge, "Hello", "World", "yellow", null, null, null, null, "http://example.com", "https://www.github.com")]
-        [InlineData(19, BadgeStyle.ForTheBadge, "Hello", "World", "yellow", null, null, null, null, "", null)]
-        [InlineData(20, BadgeStyle.ForTheBadge, "Hello", "World", "red", "blue", null, null, null, null, null)]
-        public void MakeBadge_returns_expected_svg(int id, BadgeStyle style, string leftText, string rightText, string color, string labelColor, string logo, int? logoPosition, int? logoWidth, string link1, string link2)
+        [PairwiseData]
+        public void MakeBadge_returns_expected_svg_2(
+            BadgeStyle style,
+            [CombinatorialValues("Hello")]string leftText,
+            [CombinatorialValues("World")]string rightText,
+            [CombinatorialValues("yellow", "red")]string color,
+            [CombinatorialValues(null, "", "blue")]string labelColor,
+            [CombinatorialValues(null, "", "http://example.com")]string leftLink,
+            [CombinatorialValues(null, "", "http://github.com")]string rightLink)
         {
-            var badge = Badge.MakeBadge(style, leftText, rightText, color, labelColor, logo, logoPosition, logoWidth, link1, link2);
+            var testId = GetTestCaseFileName(style, leftText, rightText, color, labelColor, leftLink, rightLink);
+
+            var badge = Badge.MakeBadge(style, leftText, rightText, color, labelColor, null, null, null, leftLink, rightLink);
             var writer = new ApprovalTextWriter(badge.ToString(), "svg");
 
-            Approvals.Verify(writer, new ApprovalNamer(id), Approvals.GetReporter());
+            Approvals.Verify(writer, new ApprovalNamer(testId), Approvals.GetReporter());
         }
 
+
+        private static string GetTestCaseFileName(BadgeStyle style, string leftText, string rightText, string color, string labelColor, string leftLink, string rightLink)
+        {
+            string GetSha256Hash(string value)
+            {
+                if (value == null)
+                {
+                    return null;
+                }
+
+                var crypt = new SHA256Managed();
+                var hash = crypt.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+                var result = new StringBuilder();
+
+                foreach (var b in hash)
+                {
+                    result.Append(b.ToString("x2"));
+                }
+                return result.ToString();
+            }
+
+
+            var nameBuilder = new StringBuilder();
+
+            nameBuilder.Append(style);
+            nameBuilder.Append("_");
+            nameBuilder.Append(leftText ?? "[null]");
+            nameBuilder.Append("_");
+            nameBuilder.Append(rightText ?? "[null]");
+            nameBuilder.Append("_");
+            nameBuilder.Append(color ?? "[null]");
+            nameBuilder.Append("_");
+            nameBuilder.Append(labelColor ?? "[null]");
+            nameBuilder.Append("_");
+
+            // Links are urls and most likely not a valid in a file name
+            // also, the full hash might result in long file names, so only the first 5 characters
+            // are included in the name which should be sufficient to make file names unique in
+            // the context of this test
+            nameBuilder.Append(GetSha256Hash(leftLink)?.Substring(0, 5) ?? "[null]");
+            nameBuilder.Append("_");
+            nameBuilder.Append(GetSha256Hash(rightLink)?.Substring(0, 5) ?? "[null]");
+
+            return nameBuilder.ToString();
+        }
     }
 }
