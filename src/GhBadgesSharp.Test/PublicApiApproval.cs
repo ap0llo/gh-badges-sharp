c1eb3da49233b18993288a59601f13515e46b4e1
Andreas Gruenwald <andreas@grynwald.net>
2019-08-24 13:45:07 +0200

    test: Add Public API approval

---
+++ src/GhBadgesSharp.Test/PublicApiApproval.cs
@@ -0,0 +1,26 @@
+﻿using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using PublicApiGenerator;
+using Xunit;
+
+namespace GhBadgesSharp.Test
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class PublicApiApproval
+    {
+        [Fact]
+        public void Assembly_must_not_have_unapproved_API_changes()
+        {
+            // ARRANGE
+            var assembly = typeof(Badge).Assembly;
+
+            // ACT
+            var publicApi = ApiGenerator.GeneratePublicApi(assembly);
+
+            // ASSERT
+            var writer = new ApprovalTextWriter(publicApi);
+            Approvals.Verify(writer, new UnitTestFrameworkNamer(), Approvals.GetReporter());
+        }
+    }
+}
