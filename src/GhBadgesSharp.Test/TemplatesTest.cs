51145811da419efebfc7f60b89e150e38d94ad76
Andreas Gruenwald <andreas@grynwald.net>
2019-08-24 14:04:38 +0200

    refactor: Make TemplateNotFoundException internal
    No external call should ever enconter this exception as in the public api, templates are passed in as enum

---
+++ src/GhBadgesSharp.Test/TemplatesTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GhBadgesSharp.Test
+{
+    public class TemplatesTest
+    {
+
+        [Theory]
+        [InlineData("flat-square")]
+        [InlineData("flat")]
+        [InlineData("for-the-badge")]
+        [InlineData("plastic")]
+        [InlineData("social")]
+        public void GetTemplate_returns_valid_template_for_known_template_names(string name)
+        {
+            Assert.NotNull(Templates.GetTemplate(name));
+        }
+    }
+}
--- src/GhBadgesSharp/TemplateNotFoundException.cs
+++ src/GhBadgesSharp/TemplateNotFoundException.cs
@@ -2,7 +2,7 @@
 
 namespace GhBadgesSharp
 {
-    public sealed class TemplateNotFoundException : Exception
+    internal sealed class TemplateNotFoundException : Exception
     {
         public TemplateNotFoundException(string templateName) : base($"Template '{templateName}' does not exist")
         { }
